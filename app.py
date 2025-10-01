d67c54173bd2c4be294a6c25fd7442d846978138
Trevor Brandon-Harris <trevor.harris@trinityschoolnyc.org>
2025-10-01 13:40:43 -0400

    updated w/ step 1

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'woc+*(,^c~L#j{;'
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -11,7 +12,17 @@ def get_db_connection():
 
 @app.route('/')
 def index():
+    # get a connection to our db
     conn = get_db_connection()
+    # gets each row
     posts = conn.execute('SELECT * FROM posts').fetchall()
+    print(posts) # Prints posts to the server's terminal
     conn.close()
     return render_template('index.html', posts=posts)
+
+# processes requests to create/ - both GET and POST request types
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    return render_template('create.html')
+
+app.run(debug=True, port=5500)
